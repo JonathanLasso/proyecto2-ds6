37ec8910805f750161c99fc85174e1ba6e477fa8
Jonathan Lasso <jonathan.lasso30@gmail.com>
2026-06-12 12:17:54 -0500

    Se implemento la segunda pantalla con sus funcionalidades

--- app/src/main/java/com/example/registrocalificaciones/ConfiguracionActivity.kt
+++ app/src/main/java/com/example/registrocalificaciones/ConfiguracionActivity.kt
@@ -1,20 +1,68 @@
 package com.example.registrocalificaciones
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.registrocalificaciones.databinding.ActivityConfiguracionBinding
 
 class ConfiguracionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityConfiguracionBinding
+    private lateinit var preferencias: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_configuracion)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityConfiguracionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        preferencias = getSharedPreferences("informacionEstudiante", Context.MODE_PRIVATE)
+        guardarDatos()
+        limpiarDatos()
+        volverAlMenuConBoton()
+        volverAlMenuConLaFlecha()
+        mostrarInformacion()
+    }
+
+    private fun guardarDatos(){
+        binding.btnGuardar.setOnClickListener {
+            val editor = preferencias.edit()
+            editor.putString("nombreCompleto", binding.etNombreCompleto.text.toString())
+            editor.putString("carrera", binding.etCarrera.text.toString())
+            editor.putString("grupo",binding.etGrupo.text.toString())
+            editor.putBoolean("notificaciones", binding.switchNotificaciones.isChecked)
+            editor.commit()
         }
     }
+
+    private fun mostrarInformacion(){
+        binding.etNombreCompleto.setText(preferencias.getString("nombreCompleto",""))
+        binding.etCarrera.setText(preferencias.getString("carrera",""))
+        binding.etGrupo.setText(preferencias.getString("grupo",""))
+    }
+
+    private fun limpiarDatos(){
+        binding.btnLimpiar.setOnClickListener {
+            binding.etNombreCompleto.setText("")
+            binding.etCarrera.setText("")
+            binding.etGrupo.setText("")
+            binding.switchNotificaciones.setChecked(false)
+        }
+    }
+
+    private fun volverAlMenuConLaFlecha(){
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun volverAlMenuConBoton(){
+        binding.btnRegresar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/registrocalificaciones/MainActivity.kt
+++ app/src/main/java/com/example/registrocalificaciones/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.registrocalificaciones
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.registrocalificaciones.databinding.ActivityMainBinding
@@ -8,25 +10,37 @@ import com.example.registrocalificaciones.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var preferencias: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        preferencias = getSharedPreferences("informacionEstudiante", Context.MODE_PRIVATE)
+        mostrarInformacionEstudiante()
+        configuracionPantallas()
     }
 
     private fun configuracionPantallas(){
         binding.btnConfigurar.setOnClickListener {
             val intent = Intent(this, ConfiguracionActivity::class.java)
+            startActivity(intent)
         }
         binding.btnRegistrar.setOnClickListener {
             val intent = Intent(this, RegistroCalificacionActivity::class.java)
+            startActivity(intent)
         }
         binding.btnHistorial.setOnClickListener {
             val intent = Intent(this, HistorialActivity::class.java)
+            startActivity(intent)
         }
         binding.btnSalir.setOnClickListener {
             finishAffinity()
         }
+    }
+
+    private fun mostrarInformacionEstudiante(){
+        binding.tvNombre.text = preferencias.getString("nombreCompleto","")
+        binding.tvCarrera.text = preferencias.getString("carrera","")
     }
 }
